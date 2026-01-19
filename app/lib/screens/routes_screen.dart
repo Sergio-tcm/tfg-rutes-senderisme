a284e2a005b019e7f0cdc65ce38c97ee02256d50
Sergio <122432878+Sergio-tcm@users.noreply.github.com>
2026-01-19 19:31:45 +0100

    Convert RoutesScreen to StatefulWidget and manage route state

--- app/lib/screens/routes_screen.dart
+++ app/lib/screens/routes_screen.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 import '../models/route_model.dart';
 import '../services/routes_service.dart';
-import 'route_detail_screen.dart';
 import '../widgets/route_card.dart';
+import 'route_detail_screen.dart';
 
-class RoutesScreen extends StatelessWidget {
+class RoutesScreen extends StatefulWidget {
   const RoutesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final RoutesService routesService = RoutesService();
-    final List<RouteModel> routes = routesService.getRoutes();
+  State<RoutesScreen> createState() => _RoutesScreenState();
+}
 
+class _RoutesScreenState extends State<RoutesScreen> {
+  final RoutesService _routesService = RoutesService();
+  late List<RouteModel> _routes;
+
+  @override
+  void initState() {
+    super.initState();
+    _routes = _routesService.getRoutes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Rutes disponibles')),
+      appBar: AppBar(
+        title: const Text('Rutes disponibles'),
+      ),
       body: ListView.builder(
-        itemCount: routes.length,
+        itemCount: _routes.length,
         itemBuilder: (context, index) {
-          final route = routes[index];
+          final route = _routes[index];
 
           return RouteCard(
             route: route,
@@ -25,7 +38,8 @@ class RoutesScreen extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => RouteDetailScreen(route: route),
+                  builder: (context) =>
+                      RouteDetailScreen(route: route),
                 ),
               );
             },
